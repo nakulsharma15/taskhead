496fa3952b2f8299352fccdba474bcb1c162ca6f
nakulsharma15 <nakuls309@gmail.com>
2022-11-25 14:04:46 +0530

    fix: grouped imports of single path in one line

--- src/Contexts/TaskContext.js
+++ src/Contexts/TaskContext.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, createContext, useContext } from "react";
+import React, { useState, createContext, useContext } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const TaskContext = createContext();
